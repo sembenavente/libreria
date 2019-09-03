a2bb9e6133c93e85cdb182f3b86b51aa590578b4
sembenavente <sembenavente@upeu.edu.pe>
2019-09-02 19:29:00 -0500

    comit 2

---
+++ src/main/java/com/benavente/springboot/backend/apirest/LibreriaSemApplication.java
@@ -0,0 +1,13 @@
+package com.benavente.springboot.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaSemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaSemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/benavente/springboot/backend/apirest/models/entity/Autor.java
@@ -0,0 +1,5 @@
+package com.benavente.springboot.backend.apirest.models.entity;
+
+public class Autor {
+
+}
